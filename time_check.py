755b80193d907411fa8f3126792ec525bf4ef3eb
Rakshit RK <rakshit.karajol@tigeranalytics.com>
2022-05-16 10:54:10 +0530

    converted time diff to hours

--- time_check.py
+++ time_check.py
@@ -5,3 +5,6 @@ time.sleep(7)
 toc = time.time()
 print("-----")
 print("time taken is---->",toc-tic," sec")
+time_diff = toc - tic
+time_diff_hrs = time_diff/3600
+print("time difference in hours--",time_diff_hrs)
