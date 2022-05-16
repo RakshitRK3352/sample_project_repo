cdf56e2d4b3dc4942f3c2e07400ec94822e9337d
Rakshit RK <rakshit.karajol@tigeranalytics.com>
2022-05-16 12:36:36 +0530

    change for testing

--- time_check.py
+++ time_check.py
@@ -4,7 +4,7 @@ tic = time.time()
 time.sleep(7)
 toc = time.time()
 print("-----")
-print("time taken is---->",toc-tic," sec")
+print("time taken is------>",toc-tic," sec")
 time_diff = toc - tic
 time_diff_hrs = time_diff/3600
 print("time difference in hours--",time_diff_hrs)
