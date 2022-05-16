e0775cf5f9aba4740fe1bd58db113303aefe90f7
Rakshit RK <rakshit.karajol@tigeranalytics.com>
2022-05-16 10:58:54 +0530

    added time difference in minutes as well

--- time_check.py
+++ time_check.py
@@ -8,3 +8,5 @@ print("time taken is---->",toc-tic," sec")
 time_diff = toc - tic
 time_diff_hrs = time_diff/3600
 print("time difference in hours--",time_diff_hrs)
+time_diff_mins = time_diff/60
+print("time taken in minutes----",time_diff_mins)
